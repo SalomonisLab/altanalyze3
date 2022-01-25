1ef124fa920453bbf5c761fbce2eb6e605db4550
Michael Kotliar <misha.kotliar@gmail.com>
2022-01-25 09:43:01 -0500

    Updated way to iterate over reads and introns. Both should be coordinate sorted

--- src/bam_to_bed/intron_retention.py
+++ src/bam_to_bed/intron_retention.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import time
+import enum
 import pysam
 import string
 import random
@@ -25,86 +26,169 @@ from multiprocessing import Pool
 #    tabix -p bed hs_ref.bed.gz
 ###############################################################################################################################################################
 
+class Segment (enum.Enum):
 
-#################################################################################################
-# DEPRECATED INSTRUCTIONS
-# keep it in case we decide to support GTF input in future
-#
-# (grep ^"#" input.gtf; grep -v ^"#" input.gtf | sort -k1,1 -k4,4n) | bgzip > input.sorted.gtf.gz
-# tabix -p gff input.sorted.gtf.gz
-#################################################################################################
+    PRIME_5 = 1
+    PRIME_3 = 2
+    INTRON = 3
+    UKNOWN = 4
 
+class Counter:
 
-def is_paired(args):
-    with pysam.AlignmentFile(args.bam, mode="rb", threads=args.threads) as bam_handler:
-        bam_iter = bam_handler.fetch()
-        for i in range(20):                       # check max 20 reads
-            try:
-                bam_handler.mate(next(bam_iter))  # Raises ValueError – if the read is unpaired or the mate is unmapped
-            except ValueError:
-                return False
-        return True
+    def __init__(self, bam, ref, threads=None):
+        self.bam = bam
+        self.ref = ref
+        self.threads = 1 if threads is None else threads
+        self.paired = self.__is_paired()
+        self.reset()
 
+    def reset(self):
+        self.overlaps = {}
+        self.cached_reads = {}
 
-def get_default_chr(args):
-    with pysam.AlignmentFile(args.bam, mode="rb", threads=args.threads) as bam_handler:
-        return [_.contig for _ in bam_handler.get_index_statistics()]
-
-
-def mate_mapped_to_intron(pileups):  # TODO
-    return pileups
-
-
-def count_alignments(column_iter, filter_by_mate=None):
-    filter_by_mate = False if filter_by_mate is None else filter_by_mate
-    collected_alignments = []
-    for column in column_iter:
-        if filter_by_mate:
-            collected_alignments.extend(mate_mapped_to_intron(column.pileups))
+    def get_segment(self, read, intron, span=None):
+        span = 0 if span is None else span
+        if read.reference_end - intron.start >= span and intron.start - read.reference_start >= span:
+            return Segment.PRIME_5
+        elif read.reference_start - intron.start >= 0 and intron.end - read.reference_end >= 0:
+            return Segment.INTRON
+        elif read.reference_end - intron.end >= span and intron.end - read.reference_start >= span:
+            return Segment.PRIME_3
         else:
-            collected_alignments.extend(column.pileups)
-    return len(collected_alignments)
+            return Segment.UKNOWN
 
+    def __is_paired(self, n_reads=None):
+        n_reads = 20 if n_reads is None else n_reads
+        with pysam.AlignmentFile(self.bam, mode="rb", threads=self.threads) as bam_handler:
+            bam_iter = bam_handler.fetch()
+            for i in range(n_reads):
+                try:
+                    bam_handler.mate(next(bam_iter))
+                except ValueError:
+                    return False
+            return True
 
-def process_contig(args, job):
-    contig = job[0]
-    location = job[1]
-    print(f"""Process {contig} into {location}""")
-    with open(location + "__" + contig, "w") as output_stream:
-        with pysam.AlignmentFile(args.bam, mode="rb", threads=args.threads) as bam_handler:
-            with pysam.TabixFile(args.ref, mode="r", parser=pysam.asBed(), threads=args.threads) as ref_handler:
-                for intron in ref_handler.fetch(contig):
-                    five_prime_counts = count_alignments(
-                        column_iter=bam_handler.pileup(       # should return exactly one item
-                            contig=contig,
-                            start=intron.start-args.span,
-                            end=intron.start-args.span+1,
-                            truncate=True                     # only columns in the exact region are returned
-                        ),
-                        filter_by_mate=args.paired
-                    )
-                    three_prime_counts = count_alignments(
-                        column_iter=bam_handler.pileup(      # should return exactly one item
-                            contig=contig,
-                            start=intron.end+args.span,
-                            end=intron.end+args.span+1,
-                            truncate=True                    # only columns in the exact region are returned
-                        ),
-                        filter_by_mate=args.paired
-                    )
-                    if five_prime_counts > 0 or three_prime_counts > 0:
-                        output_stream.write(
-                            f"""{contig}\t{intron.start}\t{intron.end}\t{intron.name}\t0\t{five_prime_counts}\t{three_prime_counts}\n"""
+    def update_overlaps(self, current_data, cached_data):
+        current_contig, current_intron_start, current_intron_end, current_intron_name, current_segment = current_data
+        if cached_data is not None:
+            cached_contig, cached_intron_start, cached_intron_end, cached_intron_name, cached_segment = cached_data
+            assert(current_contig == cached_contig)
+            if current_segment != cached_segment and current_segment in [Segment.PRIME_5, Segment.INTRON] and cached_segment in [Segment.PRIME_5, Segment.INTRON]:
+                contig = current_contig if cached_segment == Segment.INTRON else cached_contig
+                intron_start = current_intron_start if cached_segment == Segment.INTRON else cached_intron_start
+                intron_end = current_intron_end if cached_segment == Segment.INTRON else cached_intron_end
+                intron_name = current_intron_name if cached_segment == Segment.INTRON else cached_intron_name
+                default_or_current_value = self.overlaps.get(
+                    (contig, intron_start, intron_end),
+                    {
+                        "name": intron_name,
+                        "p5_count": 0,
+                        "p3_count": 0
+                    }
+                )
+                self.overlaps[(contig, intron_start, intron_end)] = {
+                    "name": intron_name,
+                    "p5_count": default_or_current_value["p5_count"] + 1,
+                    "p3_count": default_or_current_value["p3_count"]
+                }
+            elif current_segment != cached_segment and current_segment in [Segment.PRIME_3, Segment.INTRON] and cached_segment in [Segment.PRIME_3, Segment.INTRON]:
+                contig = current_contig if cached_segment == Segment.INTRON else cached_contig
+                intron_start = current_intron_start if cached_segment == Segment.INTRON else cached_intron_start
+                intron_end = current_intron_end if cached_segment == Segment.INTRON else cached_intron_end
+                intron_name = current_intron_name if cached_segment == Segment.INTRON else cached_intron_name
+                default_or_current_value = self.overlaps.get(
+                    (contig, intron_start, intron_end),
+                    {
+                        "name": intron_name,
+                        "p5_count": 0,
+                        "p3_count": 0
+                    }
+                )
+                self.overlaps[(contig, intron_start, intron_end)] = {
+                    "name": intron_name,
+                    "p5_count": default_or_current_value["p5_count"],
+                    "p3_count": default_or_current_value["p3_count"] + 1
+                }
+            else:
+                pass
+        else:
+            default_or_current_value = self.overlaps.get(
+                (current_contig, current_intron_start, current_intron_end),
+                {
+                    "name": current_intron_name,
+                    "p5_count": 0,
+                    "p3_count": 0
+                }
+            )
+            if current_segment == Segment.PRIME_5:
+                self.overlaps[(current_contig, current_intron_start, current_intron_end)] = {
+                    "name": current_intron_name,
+                    "p5_count": default_or_current_value["p5_count"] + 1,
+                    "p3_count": default_or_current_value["p3_count"]
+                }
+            elif current_segment == Segment.PRIME_3:
+                self.overlaps[(current_contig, current_intron_start, current_intron_end)] = {
+                    "name": current_intron_name,
+                    "p5_count": default_or_current_value["p5_count"],
+                    "p3_count": default_or_current_value["p3_count"] + 1
+                }
+
+    def calculate(self, contig, span):
+        with pysam.AlignmentFile(self.bam, mode="rb", threads=self.threads) as bam_handler:
+            with pysam.TabixFile(self.ref, mode="r", parser=pysam.asBed(), threads=self.threads) as ref_handler:
+                intron_iter = ref_handler.fetch(contig)
+                intron = next(intron_iter)                                                                           # get initial value from intron iterator
+                for read in bam_handler.fetch(contig):
+                    if read.reference_start - intron.end >= 0:
+                        try:
+                            intron = next(intron_iter)
+                        except StopIteration:
+                            break
+                    else:
+                        current_data = (
+                            contig,
+                            intron.start,
+                            intron.end,
+                            intron.name,
+                            self.get_segment(read, intron, span)
                         )
+                        if self.paired and read.query_name not in self.cached_reads:
+                            self.cached_reads[read.query_name] = current_data
+                        else:
+                            self.update_overlaps(
+                                current_data,
+                                self.cached_reads.get(read.query_name, None)
+                            )
+                            try:
+                                del self.cached_reads[read.query_name]
+                            except KeyError:
+                                pass
 
+    def calculate_se_old(self, contig, span):
+        with pysam.AlignmentFile(self.bam, mode="rb", threads=self.threads) as bam_handler:
+            with pysam.TabixFile(self.ref, mode="r", parser=pysam.asBed(), threads=self.threads) as ref_handler:
+                for intron in ref_handler.fetch(contig):
+                    fiv_p, thr_p = [
+                        bam_handler.count(
+                            contig=contig,
+                            start=pos,
+                            end=pos+1,
+                            read_callback=lambda r: r.reference_end - pos >= span or pos - r.reference_start >= span
+                        )
+                        for pos in [intron.start, intron.end]
+                    ]
+                    if fiv_p > 0 or thr_p > 0:
+                        self.overlaps[(contig, intron.start, intron.end)] = {
+                            "name": intron.name,
+                            "p5_count": fiv_p,
+                            "p3_count": thr_p
+                        }
 
-def collect_results(args, jobs):
-    with open(args.output + ".bed", "w") as output_stream:
-        for contig, location in jobs:
-            chunk = location + "__" + contig
-            with open(chunk, "r") as input_stream:
-                output_stream.write(input_stream.read())
-                os.remove(chunk)
+    def export(self, out):
+        print(f"Export temporary results to {out}")
+        with open(out, "w") as out_handler:
+            for (contig, start, end), v in self.overlaps.items():
+                out_handler.write(f"""{contig}\t{start}\t{end}\t{v["name"]}\t0\t{v["p5_count"]}\t{v["p3_count"]}\n""")
 
 
 def get_jobs(args):
@@ -118,6 +202,32 @@ def get_jobs(args):
             ]
 
 
+def process_contig(args, job):
+    counter = Counter(args.bam, args.ref, args.threads)
+    counter.calculate(contig=job[0], span=args.span)
+    counter.export(out=job[1]+"__"+job[0])
+
+
+def collect_results(args, jobs):
+    with open(args.output + ".bed", "w") as output_stream:
+        for contig, location in jobs:
+            chunk = location + "__" + contig
+            print(f"Collect results from {chunk}")
+            with open(chunk, "r") as input_stream:
+                output_stream.write(input_stream.read())
+                os.remove(chunk)
+
+
+def get_default_chr(args):
+    with pysam.AlignmentFile(args.bam, mode="rb", threads=args.threads) as bam_handler:
+        return [_.contig for _ in bam_handler.get_index_statistics()]
+
+
+def assert_args(args):
+    args.chr = get_default_chr(args) if len(args.chr) == 0 else args.chr
+    return args
+
+
 def normalize_args(args, skip_list=[]):
     normalized_args = {}
     for key,value in args.__dict__.items():
@@ -126,12 +236,6 @@ def normalize_args(args, skip_list=[]):
         else:
             normalized_args[key]=value
     return argparse.Namespace (**normalized_args)
-
-
-def assert_args(args):
-    args.paired = is_paired(args)
-    args.chr = get_default_chr(args) if len(args.chr) == 0 else args.chr
-    return args
 
 
 def arg_parser():
