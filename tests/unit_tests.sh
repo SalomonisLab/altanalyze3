a737921471faa5481ed8e755e55326843dfce76b
Michael Kotliar <misha.kotliar@gmail.com>
2022-05-16 17:56:58 -0400

    Save test coverage report in xml file

--- tests/unit_tests.sh
+++ tests/unit_tests.sh
@@ -4,7 +4,7 @@ pip3 install -r $DIR/unit_tests/requirements.txt
 
 $DIR/data_for_tests.sh
 
-pytest --cov=altanalyze3 --cov-append --forked $DIR/unit_tests/test_helpers.py
-pytest --cov=altanalyze3 --cov-append --forked $DIR/unit_tests/test_data.py
-pytest --cov=altanalyze3 --cov-append --forked $DIR/unit_tests/test_io.py
-pytest --cov=altanalyze3 --cov-append --forked $DIR/unit_tests/test_parser.py
+pytest --cov=altanalyze3 --cov-append --cov-report xml:cov.xml --forked $DIR/unit_tests/test_helpers.py
+pytest --cov=altanalyze3 --cov-append --cov-report xml:cov.xml --forked $DIR/unit_tests/test_data.py
+pytest --cov=altanalyze3 --cov-append --cov-report xml:cov.xml --forked $DIR/unit_tests/test_io.py
+pytest --cov=altanalyze3 --cov-append --cov-report xml:cov.xml --forked $DIR/unit_tests/test_parser.py
