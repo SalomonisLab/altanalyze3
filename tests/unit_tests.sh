d45c0dbf0842ac393dfd92f3766913412f6c756c
Michael Kotliar <misha.kotliar@gmail.com>
2022-05-16 15:53:06 -0400

    Rename files

---
+++ tests/unit_tests.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+pip3 install -r $DIR/unit_tests/requirements.txt
+
+$DIR/data_for_tests.sh
+
+pytest --cov=altanalyze --cov-append --forked $DIR/unit_tests/test_helpers.py
+pytest --cov=altanalyze --cov-append --forked $DIR/unit_tests/test_data.py
+pytest --cov=altanalyze --cov-append --forked $DIR/unit_tests/test_io.py
--- tests/unit_tests/run_unit_tests.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-pip3 install -r $DIR/test_requirements.txt
-
-$DIR/../prepare_data_for_tests.sh
-
-pytest --cov=altanalyze --cov-append --forked $DIR/test_helpers.py
-pytest --cov=altanalyze --cov-append --forked $DIR/test_data.py
-pytest --cov=altanalyze --cov-append --forked $DIR/test_io.py
