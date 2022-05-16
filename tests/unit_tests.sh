67ba3fb85a57fd4140fe15fb63c5ac799528e519
Michael Kotliar <misha.kotliar@gmail.com>
2022-05-16 17:35:14 -0400

    Collecting reports from multiple tests

--- tests/unit_tests.sh
+++ tests/unit_tests.sh
@@ -4,6 +4,7 @@ pip3 install -r $DIR/unit_tests/requirements.txt
 
 $DIR/data_for_tests.sh
 
-pytest --cov=altanalyze --cov-append --forked $DIR/unit_tests/test_helpers.py
-pytest --cov=altanalyze --cov-append --forked $DIR/unit_tests/test_data.py
-pytest --cov=altanalyze --cov-append --forked $DIR/unit_tests/test_io.py
+pytest --cov=altanalyze3 --cov-append --forked $DIR/unit_tests/test_helpers.py
+pytest --cov=altanalyze3 --cov-append --forked $DIR/unit_tests/test_data.py
+pytest --cov=altanalyze3 --cov-append --forked $DIR/unit_tests/test_io.py
+pytest --cov=altanalyze3 --cov-append --forked $DIR/unit_tests/test_parser.py
