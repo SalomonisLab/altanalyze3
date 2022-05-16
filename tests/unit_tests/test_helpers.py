6a856bbb4e54bfe6a34ebd0e120829b575662636
Michael Kotliar <misha.kotliar@gmail.com>
2022-05-16 12:35:41 -0400

    More unit tests

--- tests/unit_tests/test_helpers.py
+++ tests/unit_tests/test_helpers.py
@@ -1,6 +1,10 @@
+import time
 import pytest
 import pathlib
-from altanalyze3.utilities.helpers import get_md5_sum
+from altanalyze3.utilities.helpers import (
+    get_md5_sum,
+    get_tmp_suffix
+)
 
 
 DATA_FOLDER = pathlib.Path(__file__).resolve().parents[1].joinpath("data")
@@ -15,3 +19,13 @@ DATA_FOLDER = pathlib.Path(__file__).resolve().parents[1].joinpath("data")
 def test_get_md5_sum(location, control_md5_sum):
     calculated_md5_sum = get_md5_sum(DATA_FOLDER.joinpath(location))
     assert calculated_md5_sum == control_md5_sum
+
+
+@pytest.mark.parametrize(
+    "length_without_dot",
+    [10, 12, 3]
+)
+def test_get_tmp_suffix(length_without_dot):
+    tmp_suffix = get_tmp_suffix(length_without_dot)
+    print(tmp_suffix)
+    assert len(tmp_suffix) == length_without_dot + 1
