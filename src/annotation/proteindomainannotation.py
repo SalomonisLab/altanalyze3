1bbcbb1d8e181807a8d2482884ab61cb17f3d0f7
Preeti Singh <singhpreeti236.com>
2022-05-19 07:46:55 -0400

    clean the code

--- src/annotation/proteindomainannotation.py
+++
@@ -1,8 +0,0 @@
-"""
-    Template function for mapping a feature to the associated transcript(s), then 
-    converting the given coordinates - by getting the data from Ensemble
-    Args:
-        protein id (ENSP..)
-    Returns:
-        list: of converted coordinates, mapped to a `Feature` instance
-"""
