78df994840238fdf53f2904828db646e58d84ded
Michael Kotliar <misha.kotliar@gmail.com>
2022-07-25 17:38:39 -0400

    Not imporant changes

--- altanalyze3/utilities/constants.py
+++ altanalyze3/utilities/constants.py
@@ -2,32 +2,34 @@ import enum
 from collections import namedtuple
 
 
-MAIN_CRH = ["chr1",
-            "chr2",
-            "chr3",
-            "chr4",
-            "chr5",
-            "chr6",
-            "chr7",
-            "chr8",
-            "chr9",
-            "chr10",
-            "chr11",
-            "chr12",
-            "chr13",
-            "chr14",
-            "chr15",
-            "chr16",
-            "chr17",
-            "chr18",
-            "chr19",
-            "chr20",
-            "chr21",
-            "chr22",
-            "chrX",
-            "chrY",
-            "chrM"
-            ]
+MAIN_CRH = [
+    "chr1",
+    "chr2",
+    "chr3",
+    "chr4",
+    "chr5",
+    "chr6",
+    "chr7",
+    "chr8",
+    "chr9",
+    "chr10",
+    "chr11",
+    "chr12",
+    "chr13",
+    "chr14",
+    "chr15",
+    "chr16",
+    "chr17",
+    "chr18",
+    "chr19",
+    "chr20",
+    "chr21",
+    "chr22",
+    "chrX",
+    "chrY",
+    "chrM"
+]
+
 
 class IntRetCat (enum.IntEnum):
     """
