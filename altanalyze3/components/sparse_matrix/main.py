8ad3a23518bd6301e887e5590002503d217a76bb
Preeti Singh <singhpreeti236.com>
2022-07-26 15:37:57 -0400

    parse each file and make a sparse matrix

--- altanalyze3/components/sparse_matrix/main.py
+++ altanalyze3/components/sparse_matrix/main.py
@@ -6,6 +6,8 @@ from scipy.sparse import csr_matrix, dok_matrix
 import time
 from timeit import default_timer as timer
 from multiprocessing import Pool, cpu_count
+import anndata as ad
+import h5py
 
 class SparseMatrix:
 
@@ -143,99 +145,56 @@ class JunctionCoordinateCalculation:
 
         
     
-def readBamFiles(self):
-    '''to d0 - dont forget there re two sbed files per sample - one for intron and other for exon-exon junciotn'''
-    bamfiles = ['/Users/sin9gp/altanalyze3/tests/data/sample1.bed','/Users/sin9gp/altanalyze3/tests/data/sample2.bed']
-    sampleids = []
-    for file in bamfiles:
-        # print(sampleids)
-        bamdata = pd.read_csv(file, sep='\t')
-        df = pd.DataFrame(bamdata)
-        sampleids = [os.path.basename(file)]
-        junctionCoordinates = self.getJunctionAndCounts(file)
-        # calculate matrix dimensions
-        M = self.totalJunctionCoordinatecount(df.iloc[:,[3]]) #number of rows
-        N = self.totalSampleFiles(bamfiles) #number of columns
-    dok_sparse = dok_matrix((M,N))
-    data = []
-    for col in sampleids:
-        for junction in junctionCoordinates:
-            for val in junction.values():
-                data.append(val)
-                dok_sparse[junction,col] = val
-    
-    csr = dok_sparse.tocsr()
-    print(csr)
-
-  
-
-# def main():
-#     '''
-#     Use Python filesystem to load the sample files
-#     Use Multi-processing to read and divide them by chromosome
-#     call sparse matrix function on each process
-#     '''
-#     pool = Pool(4) # number of cores you want to use
-#     file_list = os.listdir("/Users/sin9gp/altanalyze3/tests/data/bed")
-#     df_list = pool.map(reader, file_list) #creates a list of the loaded df's
-#     df = pd.concat(df_list) # concatenates all the df's into a single df
-
-#     for file in file_list:
-#         if file.endswith(".bed"):
-#             print("hello", file)
-
-   
-def square(n):
-
-    time.sleep(2)
-
-    return n * n
-
-
-def main():
-
-    start = timer()
-
-    print(f'starting computations on {cpu_count()} cores')
-
-    values = (2, 4, 6, 8)
-
-    with Pool() as pool:
-        res = pool.map(square, values)
-        print(res)
-
-    end = timer()
-    print(f'elapsed time: {end - start}')
-            
-            
-
+    def readBamFiles(self):
+        '''to d0 - dont forget there re two sbed files per sample - one for intron and other for exon-exon junciotn'''
+        # TO - DO os.join (join the path to the name)
+        juncfiles = ['/Users/sin9gp/altanalyze3/tests/data/bed/sample1.bed','/Users/sin9gp/altanalyze3/tests/data/bed/sample2.bed']
+        sampleids = os.listdir("/Users/sin9gp/altanalyze3/tests/data/bed")
+      
+        for file in juncfiles:
+            # print(sampleids)
+            bamdata = pd.read_csv(file, sep='\t')
+            df = pd.DataFrame(bamdata)
+            junctionCoordinates = self.getJunctionAndCounts(file)
+            # calculate matrix dimensions
+            M = self.totalJunctionCoordinatecount(df.iloc[:,[3]]) #number of rows
+            N = self.totalSampleFiles(juncfiles) #number of columns
+        dok_sparse = dok_matrix((M,N))
         
-    
-if __name__ == '__main__':
-    '''
-    Pool object which offers a convenient means of parallelizing the execution of a function across 
-    multiple input values, distributing the input data across processes
-
-    Map() - This method chops the iterable into a number of chunks which it submits to the process pool as separate tasks.
-    The (approximate) size of these chunks can be specified by setting chunksize to a positive integer.
-    '''
-    
-    
-    # junctionCoordinates = JunctionCoordinateCalculation()
-    # file_list = os.listdir("/Users/sin9gp/altanalyze3/tests/data/bed")
-    # print(file_list)
-    # junctionCoordinates.reader(file_list)
-    main()
-
+        for sampleid in sampleids:
+            for junction in junctionCoordinates:
+                for spliceevents in junction.values():
+                    dok_sparse[junction,sampleid] = spliceevents
         
-   
-# benchmark - time the matrix conversion 
-# convert to csr_matrix?
+        csr = dok_sparse.tocsr()
+        print(csr)
+    
+    def geth5adFile(self):
+        file_list = os.listdir("/Users/sin9gp/altanalyze3/tests/data/bed")
+        psi = self.readBamFiles()
+        for sampleFile in file_list:
+            bamdata = pd.read_csv(sampleFile, sep='\t')
+            junctions = pd.DataFrame(bamdata).iloc[:,[3]].values.tolist()
+            coordinates = pd.DataFrame(bamdata).iloc[:,[2]].values.tolist()
+        adata = ad.AnnData(X=psi,var=pd.DataFrame(index=junctions),obs=pd.DataFrame(index=coordinates))
+        adata.write('test.h5ad')
+
+
+
+# benchmark - time the matrix conversion - cant run it on cluster/ 
+# convert to csr_matrix? - Done
 # test this with 6-8 samples (run on the cluster)
             
-        
-
-
+#July 21: 
+# module2 - annotation -> row id  -> divide by chromosome (per process)
+# module1 -  sparse matrix - (read one file per process) divide by samples (per process)
+# benchmark on cluster
+# use csr_array and dok_array instead of matrix
+# __junction.txt - function should be able to handle __
+# there will be two files per sample - so get the file names only once - you can use set(file_list)
+#you can visualize PSI matrix
+junctionCoordinate = JunctionCoordinateCalculation()
+junctionCoordinate.readBamFiles()
 
 
 
--- altanalyze3/components/sparse_matrix/main_new.py
+++ altanalyze3/components/sparse_matrix/main_new.py
@@ -6,44 +6,54 @@ from scipy.sparse import csr_matrix, dok_matrix
 import time
 from timeit import default_timer as timer
 from multiprocessing import Pool, cpu_count
+from csv import DictReader
+from csv import reader
 
-def square(n):
+def createMatrix(row):
+    #print('/Users/sin9gp/altanalyze3/tests/data/bed/' + lines)
+    M = len(row) #number of rows
+    N = 5 #number of columns
+    dok_sparse = dok_matrix((M,N))
+    data = []
+    file_list = os.listdir("/Users/sin9gp/altanalyze3/tests/data/bed")
+    for i in range(0,len(file_list)):
+        for junction in row[3]:
+            data.append(junction)
+            dok_sparse[junction,i] = row[3]
+    return dok_sparse
 
-    time.sleep(2)
-
-    return n * n
-
+    
 
 def main():
-
-    start = timer()
-    print(f'starting computations on {cpu_count()} cores')
-    file_list = os.listdir("/Users/sin9gp/altanalyze3/tests/data/bed")
-    values = (2, 4, 6, 8)
-
-    with Pool() as pool:
-        res = pool.map(square, values)
-        print(res)
-
-    end = timer()
-    print(f'elapsed time: {end - start}')
-            
-            
-
-        
-    
-if __name__ == '__main__':
     '''
-    Pool object which offers a convenient means of parallelizing the execution of a function across 
+    Pool object which offers a means of parallelizing the execution of a function across 
     multiple input values, distributing the input data across processes
 
     Map() - This method chops the iterable into a number of chunks which it submits to the process pool as separate tasks.
     The (approximate) size of these chunks can be specified by setting chunksize to a positive integer.
     '''
+    start = timer()
+    print(f'starting computations on {cpu_count()} cores')
+    file_list = os.listdir("/Users/sin9gp/altanalyze3/tests/data/bed")
     
+    p = Pool(processes=cpu_count() - 7)
+    print(p)
+    for file in file_list:
+        with open('/Users/sin9gp/altanalyze3/tests/data/bed/' + file) as f:
+            csv_dict_reader = reader(f, delimiter='\t')
+            #print(row[0])
+            with Pool() as pool:
+                res = pool.map(createMatrix, csv_dict_reader)
+                print(res)
+
+    end = timer()
+    print(f'elapsed time: {end - start}')
+
+
+#fix this divide process - divide by sample (each sample has two files (intron and junction files))
+            
+
+        
     
-    # junctionCoordinates = JunctionCoordinateCalculation()
-    # file_list = os.listdir("/Users/sin9gp/altanalyze3/tests/data/bed")
-    # print(file_list)
-    # junctionCoordinates.reader(file_list)
+if __name__ == '__main__':
     main()
