d89f06a00f90863ae0dffedb87d46d912650ee06
Preeti Singh <singhpreeti236.com>
2022-08-01 15:34:29 -0400

    getting splice annotation from junction file and gene model file

--- altanalyze3/components/merging/main.py
+++ altanalyze3/components/merging/main.py
@@ -1,8 +1,12 @@
+from operator import truediv
 import pandas as pd
 import os
 
 #path for gene_model and junction files
 
+junction_dict = {}
+exon_dict = {}
+gene_dict = {}
 
 def sampleBEDFileAnnotation(gene_model_all, junction_dir, intron_dir):
     exon_dict, gene_dict,junction_dict = importJunctionInfo(gene_model_all)
@@ -16,10 +20,7 @@ def importJunctionInfo(gene_model_all):
     2. exon_dict
     3. gene_dict
     '''
-    junction_dict = {}
-    exon_dict = {}
-    gene_dict = {}
-
+    
 
     gene_model_df = pd.read_csv(gene_model_all,sep='\t',header=None, names=[
             "gene_id", "chr", "strand","exon_region_id", "start", "stop", "exon_annotations"])    
@@ -42,8 +43,10 @@ def importJunctionInfo(gene_model_all):
 
    
 def get_exon_annotation(chr, start,stop,exonid):
-    if(exonid.startswith('E')):
-        return exonid + '.' + str(chr) + '_' + str(start) + '_'+  str(stop)
+    exon_start = chr + ':' + str(start) 
+    exon_stop = chr + ':' + str(stop)
+    exon_id =  exonid + '.' + str(chr)
+    return {'exon_start':exon_start, 'exon_stop': exon_stop,'exon_id':exon_id}
 
 
 def importJunctionFromBED(junction_dir, junction_dict):
@@ -51,7 +54,7 @@ def importJunctionFromBED(junction_dir, junction_dict):
     This function imports junction data from BAM derived exon junction text files
     '''
     junction_coordinate_BAM_dict = {}
-    
+
     for junctionfilename in os.listdir(junction_dir):
         junctionfile = os.path.join(junction_dir, junctionfilename)
         junction_coordinate_BAM_dict = initialBAMJunctionImport(junctionfile)
@@ -69,12 +72,11 @@ def importJunctionFromBED(junction_dir, junction_dict):
             startAnnotation = junction_dict[chr,start]
         if junction_dict.get(tar_stop_tup) != None:
             stopAnnotation = junction_dict[chr,stop]
-        junctionAnnotation = getJunctionAnnotation(startAnnotation,stopAnnotation)
+        junctionAnnotation = JunctionAnnotation(startAnnotation,stopAnnotation)
         junction_coordinate_BAM_dict[chr,start,stop] = junctionAnnotation
-        #print(junctionAnnotation)
+        print(junctionAnnotation)
+    return junction_coordinate_BAM_dict
 
-def getJunctionAnnotation(startannotation, stopannotation):
-    return 1
 
 def initialBAMJunctionImport(junctionfile):
     interimJunctionCoordinates={}
@@ -86,6 +88,75 @@ def initialBAMJunctionImport(junctionfile):
             interimJunctionCoordinates[(row.chr,row.start,row.stop)] = []
     return interimJunctionCoordinates
     
+class JunctionAnnotation:
+    def __init__(self,chr,start,stop,start_annotation,stop_annotation):
+        self.chr = chr,
+        self.start = start,
+        self.stop = stop,
+        self.start_annotation = start_annotation
+        self.stop_annotation = stop_annotation
+    
+    def stopAnnotation(self):
+        if self.stop_annotation == None and self.startAnnotation() == None:
+            self.stop_annotation = 'NA'
+        elif self.stop_annotation == None:
+            candidateGene = self.findGene(self.startAnnotation())
+            self.stop_annotation = self.annotateSpliceSite(self.chr,self.stop,candidateGene)
+        return self.stop_annotation
+    
+    def startAnnotation(self):
+        if self.stop_annotation == None and self.start_annotation == None:
+            self.start_annotation = 'NA'
+        print('start annotation', self.start_annotation)
+        return self.start_annotation
+
+    def findGene(self,exonAnnotation):
+        return str.split(exonAnnotation,':')[0]
+    
+    def annotateSpliceSite(self,chr,coordinate,candidateGene):
+        gene_start = gene_dict[candidateGene][0]
+        gene_stop = gene_dict[candidateGene][-1]
+        print("we are gene start and stop inside annotat splice site", gene_start, gene_stop)
+
+        inRange = self.coordinateInRange(coordinate,gene_start,gene_stop, buffer = 0)
+
+        if inRange == False:
+            self.coordinateInRange(coordinate,gene_start,gene_stop,buffer = 2000)
+
+        if inRange: ### Yes the candidate gene is the correct gene
+            candidateFound = False ### preset when initially looking (always false to start)
+            for ea in exon_dict[candidateGene]:
+                exon_start = ea['exon_start']
+                exon_stop = ea['exon_stop']
+                exon_id = ea['exon_id']
+                if exon_id.startswith('I'):
+                    intron_status = True
+                    buffer = 0
+                else:
+                    intron_status = False
+                    buffer = 50
+                inRange = self.coordinateInRange(coordinate,exon_start,exon_stop, buffer)
+                if inRange:
+                    annotation = exon_id + '_' + coordinate
+                    if intron_status == False:
+                        return annotation
+                        break
+                    else:
+                        ### This means that the junction is within intron 
+                        # but we need to check if it is in the next exon with 50nt spacer
+                        candidateFound = True
+                        continue
+                else:
+                    if candidateFound:
+                        ### annotated as a novel intron splice in the last loop
+                        return annotation 
+                    pass
+
+    def coordinateInRange(coordinate, start,stop, buffer):
+        if coordinate >= start - buffer and coordinate <= stop + buffer:
+            return True
+        return False
+
 
 
 if __name__ == '__main__':
