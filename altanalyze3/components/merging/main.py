383096eb7445963302ea313e620194a4c0605d49
Preeti Singh <singhpreeti236.com>
2022-08-05 18:56:39 -0400

    check if coordinate is in range for both positive and negative strand

--- altanalyze3/components/merging/main.py
+++ altanalyze3/components/merging/main.py
@@ -8,88 +8,91 @@ junction_dict = {}
 exon_dict = {}
 gene_dict = {}
 
-class 
-def sampleBEDFileAnnotation(gene_model_all, junction_dir, intron_dir):
-    exon_dict, gene_dict,junction_dict = importJunctionInfo(gene_model_all)
-    importJunctionFromBED(junction_dir, junction_dict)
-
-
-def importJunctionInfo(gene_model_all):
-    '''
-    This method will take gene_model as input and create three dictionaries
-    1. junction_dict
-    2. exon_dict
-    3. gene_dict
-    '''
+class DataCollection:
     
-
-    gene_model_df = pd.read_csv(gene_model_all,sep='\t',header=None, names=[
-            "gene_id", "chr", "strand","exon_region_id", "start", "stop", "exon_annotations"])    
-    #junction_dict = gene_model_df.set_index('chr','start').to_dict()['gene_id']
-    for idx,row in gene_model_df.iterrows():
-        junction_dict[(row.chr,row.start)] = row.gene_id + ':' + row.exon_region_id
-        junction_dict[(row.chr,row.stop)] = row.gene_id + ':' + row.exon_region_id
-        exon_annotation = get_exon_annotation(row.chr,row.start,row.stop,row.exon_region_id)
-        if row.gene_id in exon_dict.keys():
-            exon_dict[row.gene_id].append(exon_annotation)
-            gene_dict[row.gene_id].append(row.start)
-            gene_dict[row.gene_id].append(row.stop)
-        else:
-            exon_dict[row.gene_id] = [exon_annotation]
-            gene_dict[row.gene_id] = [row.start,row.stop]
-    # for gene in gene_dict:
-    #     gene_dict[gene].sort()
-    #print(junction_dict)
-    return exon_dict, gene_dict, junction_dict
-
-   
-def get_exon_annotation(chr, start,stop,exonid):
-    exon_start = start
-    exon_stop = stop
-    exon_id =  exonid
-    chr = chr
-    return {'exon_start':exon_start, 'exon_stop': exon_stop,'exon_id':exon_id,'chr':chr}
+    def sampleBEDFileAnnotation(self,gene_model_all, junction_dir, intron_dir):
+        exon_dict, gene_dict,junction_dict = self.importJunctionInfo(gene_model_all)
+        self.importJunctionFromBED(junction_dir, junction_dict)
 
 
-def importJunctionFromBED(junction_dir, junction_dict):
-    '''
-    This function imports junction data from BAM derived exon junction text files
-    '''
-    junction_coordinate_BAM_dict = {}
+    def importJunctionInfo(self,gene_model_all):
+        '''
+        This method will take gene_model as input and create three dictionaries
+        1. junction_dict
+        2. exon_dict
+        3. gene_dict
+        '''
+        
 
-    for junctionfilename in os.listdir(junction_dir):
-        junctionfile = os.path.join(junction_dir, junctionfilename)
-        junction_coordinate_BAM_dict.append(initialBAMJunctionImport(junctionfile))
-
-    for (chr,start, stop) in junction_coordinate_BAM_dict.keys():
-        unprefixedchr = chr.removeprefix('chr')
-        startAnnotation = None
-        stopAnnotation = None
-        #print(unprefixedchr,start)
+        gene_model_df = pd.read_csv(gene_model_all,sep='\t',header=None, names=[
+                "gene_id", "chr", "strand","exon_region_id", "start", "stop", "exon_annotations"])    
+        #junction_dict = gene_model_df.set_index('chr','start').to_dict()['gene_id']
+        for idx,row in gene_model_df.iterrows():
+            junction_dict[(row.chr,row.start)] = row.gene_id + ':' + row.exon_region_id
+            junction_dict[(row.chr,row.stop)] = row.gene_id + ':' + row.exon_region_id
+            exon_annotation = self.get_exon_annotation(row.chr,row.start,row.stop,row.exon_region_id)
+            if row.gene_id in exon_dict.keys():
+                exon_dict[row.gene_id].append(exon_annotation)
+                gene_dict[row.gene_id].append(row.start)
+                gene_dict[row.gene_id].append(row.stop)
+            else:
+                exon_dict[row.gene_id] = [exon_annotation]
+                gene_dict[row.gene_id] = [row.start,row.stop]
+        # for gene in gene_dict:
+        #     gene_dict[gene].sort()
         #print(junction_dict)
-        tar_start_tup = (unprefixedchr,start)
-        tar_stop_tup = (unprefixedchr,stop)
-        if junction_dict.get(tar_start_tup) != None:
-            print("exists")
-            startAnnotation = junction_dict[chr,start]
-        if junction_dict.get(tar_stop_tup) != None:
-            stopAnnotation = junction_dict[chr,stop]
-        junctionAnnotation = JunctionAnnotation(startAnnotation,stopAnnotation)
-        junction_coordinate_BAM_dict[chr,start,stop] = junctionAnnotation
-        print(junctionAnnotation)
-    return junction_coordinate_BAM_dict
+        return exon_dict, gene_dict, junction_dict
 
-
-def initialBAMJunctionImport(junctionfile):
-    interimJunctionCoordinates={}
-    junction_df = pd.read_csv(junctionfile,sep='\t',header=None, names=[
-            "chr", "start", "stop","annotation", "counts","strand"])
-    for idx,row in junction_df.iterrows():
-        # check if it's a valid chr
-        if(len(row.chr) <= 5):
-            interimJunctionCoordinates[(row.chr,row.start,row.stop)] = []
-    return interimJunctionCoordinates
     
+    def get_exon_annotation(chr, start,stop,exonid):
+        exon_start = start
+        exon_stop = stop
+        exon_id =  exonid
+        chr = chr
+        return {'exon_start':exon_start, 'exon_stop': exon_stop,'exon_id':exon_id,'chr':chr}
+
+
+    def importJunctionFromBED(self,junction_dir, junction_dict):
+        '''
+        This function imports junction data from BAM derived exon junction text files
+        '''
+        junction_coordinate_BAM_dict = {}
+
+        for junctionfilename in os.listdir(junction_dir):
+            junctionfile = os.path.join(junction_dir, junctionfilename)
+            junction_coordinate_BAM_dict.append(self.initialBAMJunctionImport(junctionfile))
+        
+        print(junction_coordinate_BAM_dict)
+
+        for (chr,start, stop) in junction_coordinate_BAM_dict.keys():
+            unprefixedchr = chr.removeprefix('chr')
+            startAnnotation = None
+            stopAnnotation = None
+            #print(unprefixedchr,start)
+            #print(junction_dict)
+            tar_start_tup = (unprefixedchr,start)
+            tar_stop_tup = (unprefixedchr,stop)
+            if junction_dict.get(tar_start_tup) != None:
+                print("exists")
+                startAnnotation = junction_dict[chr,start]
+            if junction_dict.get(tar_stop_tup) != None:
+                stopAnnotation = junction_dict[chr,stop]
+            junctionAnnotation = JunctionAnnotation(startAnnotation,stopAnnotation)
+            junction_coordinate_BAM_dict[chr,start,stop] = junctionAnnotation
+            print(junctionAnnotation)
+        return junction_coordinate_BAM_dict
+
+
+    def initialBAMJunctionImport(junctionfile):
+        interimJunctionCoordinates={}
+        junction_df = pd.read_csv(junctionfile,sep='\t',header=None, names=[
+                "chr", "start", "stop","annotation", "counts","strand"])
+        for idx,row in junction_df.iterrows():
+            # check if it's a valid chr
+            if(len(row.chr) <= 5):
+                interimJunctionCoordinates[(row.chr,row.start,row.stop)] = []
+        return interimJunctionCoordinates
+        
 class JunctionAnnotation:
     def __init__(self,chr,start,stop,start_annotation,stop_annotation):
         self.chr = chr,
@@ -155,9 +158,13 @@ class JunctionAnnotation:
             inRange = self.coordinateInRange(coordinate,gene_start,gene_stop,buffer = 2000)
 
     def coordinateInRange(coordinate, start,stop, buffer):
-        if coordinate >= start - buffer and coordinate <= stop + buffer:
-            return True
-        return False
+        if start < stop:
+            if coordinate >= start - buffer and coordinate <= stop + buffer:
+                return True
+        else:
+            if coordinate >= start + buffer and coordinate <= stop - buffer:
+                return True
+        
 
 
 
@@ -167,6 +174,7 @@ if __name__ == '__main__':
     gene_model_file = f"{genemodelpath}/{file}"
     junction_dir = "/Users/sin9gp/altanalyze3/tests/data/junction_dir/"
     intron_dir = "/Users/sin9gp/altanalyze3/tests/data/intron_dir/"
-    sampleBEDFileAnnotation(gene_model_file, junction_dir, intron_dir)
+    dataCollection = DataCollection()
+    dataCollection.sampleBEDFileAnnotation(gene_model_file, junction_dir, intron_dir)
     # importJunctionInfo(gene_model_all=gene_model_file)
     # importJunctionFromBED(junctiondir)
