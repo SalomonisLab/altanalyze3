4e710a7daaf394b06a8cf716656236479f0430de
Preeti Singh <singhpreeti236.com>
2022-08-12 17:14:49 -0400

    initial commits for splice annotation

--- altanalyze3/components/merging/main_new.py
+++ altanalyze3/components/merging/main_new.py
@@ -1,8 +1,9 @@
 from genericpath import exists
 from operator import truediv
-import pandas as pd
+
 import os
 from functools import cache
+import logging
 
 class   JunctionAnnotation:
     def __init__(self):
@@ -19,7 +20,7 @@ class   JunctionAnnotation:
         junction_dict = self.generate_junction_dict(gene_model_all)
         junction_coordinate_BAM_listof_dicts = []
         for junction_file in junction_files:
-            print("Reading junction file" + '' + junction_file)
+            logging.info("Reading junction file" + '' + junction_file)
             with open(junction_dir + junction_file) as f:
                 
                 for junction in f:
@@ -32,11 +33,11 @@ class   JunctionAnnotation:
                     if junction_dict.get(start_tar_tup) != None:
                         
                         self.start_annotation = {'exon_region_id':junction_dict[start_tar_tup]['exon_region_id'],
-                        'gene_id':junction_dict[start_tar_tup]['gene_id'],'start':junction_start}
+                        'gene_id':junction_dict[start_tar_tup]['gene_id'],'junction_start':junction_start}
                         #print(self.start_annotation)
                     if junction_dict.get(stop_tar_tup) != None:
                         self.stop_annotation = {'exon_region_id':junction_dict[stop_tar_tup]['exon_region_id'],
-                        'gene_id':junction_dict[stop_tar_tup]['gene_id'],'stop':junction_stop}
+                        'gene_id':junction_dict[stop_tar_tup]['gene_id'],'junction_stop':junction_stop}
                     #print(self.start_annotation,self.stop_annotation)
                     self.junction_coordinate_BAM_dict[(chr,junction_start,junction_stop)] = self.splice_annotations(self.start_annotation,self.stop_annotation)
         #print(self.junction_coordinate_BAM_dict)
@@ -48,20 +49,22 @@ class   JunctionAnnotation:
     def generate_junction_dict(self,gene_model_all):
         junction_dict = {}
         gene_model_df = pd.read_csv(gene_model_all,sep='\t',header=None, names=[
-                "gene_id", "chr", "strand","exon_region_id", "start", "stop", "exon_annotations"]) 
-        print("Generating junction dictionary from gene model(hg38).....")
+                "gene_id", "chr", "strand","exon_region_id", "start", "stop", "exon_annotations"])
+        print(gene_model_df)
+        logging.info("Generating junction dictionary from gene model(hg38).....")
         
-        for idx,row in gene_model_df.iterrows():
-            
+        for idx,row in gene_model_df.iterrows():           
             junction_dict[(row.chr,row.start)] = {'gene_id':row.gene_id, 'exon_region_id':row.exon_region_id}
             junction_dict[(row.chr,row.stop)] = {'gene_id':row.gene_id, 'exon_region_id':row.exon_region_id}
         
-        print("Finished generating junction map from gene model")
+        print("hello")
+        logging.info("Finished generating junction map from gene model")
+
         return junction_dict
     
     def splice_annotations(self,start_ann,stop_ann):
         annotations = {}
-        #print("splice annotation")
+        annotation = start_ann['exon_region_id'] + '_' + start_ann['start']   
         return annotations
     
 
