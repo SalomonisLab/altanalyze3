ce759564e8a79dec4fb4f83a7d0dd9b7bf1a1783
Preeti Singh <singhpreeti236.com>
2022-08-13 20:23:06 -0400

    check if start and stop annotations are not None and then find the splice annotations resepectively

--- altanalyze3/components/merging/main_new.py
+++ altanalyze3/components/merging/main_new.py
@@ -1,6 +1,6 @@
 from genericpath import exists
 from operator import truediv
-
+import pandas as pd
 import os
 from functools import cache
 import logging
@@ -17,7 +17,7 @@ class   JunctionAnnotation:
         Output: map of annotations for a given junction
         '''
         junction_files = os.listdir(junction_dir)
-        junction_dict = self.generate_junction_dict(gene_model_all)
+        gene_model_dict = self.generate_gene_model_dict(gene_model_all)
         junction_coordinate_BAM_listof_dicts = []
         for junction_file in junction_files:
             logging.info("Reading junction file" + '' + junction_file)
@@ -29,47 +29,94 @@ class   JunctionAnnotation:
                     junction_stop = int(junction.split('\t')[2])
                     start_tar_tup = (chr, junction_start)
                     stop_tar_tup = (chr, junction_stop)
-                    #print(start_tar_tup)
-                    if junction_dict.get(start_tar_tup) != None:
+                    
+                    if gene_model_dict.get(start_tar_tup) != None:
+                        self.start_annotation = {'exon_region_id':gene_model_dict[start_tar_tup]['exon_region_id'],
+                        'gene_id':gene_model_dict[start_tar_tup]['gene_id'],'junction_start':junction_start, 'ref_stop':gene_model_dict[start_tar_tup]['ref_stop']}
                         
-                        self.start_annotation = {'exon_region_id':junction_dict[start_tar_tup]['exon_region_id'],
-                        'gene_id':junction_dict[start_tar_tup]['gene_id'],'junction_start':junction_start}
-                        #print(self.start_annotation)
-                    if junction_dict.get(stop_tar_tup) != None:
-                        self.stop_annotation = {'exon_region_id':junction_dict[stop_tar_tup]['exon_region_id'],
-                        'gene_id':junction_dict[stop_tar_tup]['gene_id'],'junction_stop':junction_stop}
-                    #print(self.start_annotation,self.stop_annotation)
-                    self.junction_coordinate_BAM_dict[(chr,junction_start,junction_stop)] = self.splice_annotations(self.start_annotation,self.stop_annotation)
-        #print(self.junction_coordinate_BAM_dict)
+                    if gene_model_dict.get(stop_tar_tup) != None:
+                        self.stop_annotation = {'exon_region_id':gene_model_dict[stop_tar_tup]['exon_region_id'],
+                        'gene_id':gene_model_dict[stop_tar_tup]['gene_id'],'junction_stop':junction_stop, 'ref_start':junction_start}
+                    
+                    self.junction_coordinate_BAM_dict[(chr,junction_start,junction_stop)] = Splice_Site_Annotation(self.start_annotation,self.stop_annotation)
                    
         return self.junction_coordinate_BAM_dict
 
    
     @cache
-    def generate_junction_dict(self,gene_model_all):
-        junction_dict = {}
+    def generate_gene_model_dict(self,gene_model_all):
+        '''
+        This helper function genr
+        '''
+        gene_model_dict = {}
+        gene_model_exon_dict = {}
+        ea = []
         gene_model_df = pd.read_csv(gene_model_all,sep='\t',header=None, names=[
                 "gene_id", "chr", "strand","exon_region_id", "start", "stop", "exon_annotations"])
         print(gene_model_df)
         logging.info("Generating junction dictionary from gene model(hg38).....")
         
         for idx,row in gene_model_df.iterrows():           
-            junction_dict[(row.chr,row.start)] = {'gene_id':row.gene_id, 'exon_region_id':row.exon_region_id}
-            junction_dict[(row.chr,row.stop)] = {'gene_id':row.gene_id, 'exon_region_id':row.exon_region_id}
-        
+            gene_model_dict[(row.chr,row.start)] = {'gene_id':row.gene_id, 'exon_region_id':row.exon_region_id, 'start':row.start}
+            gene_model_dict[(row.chr,row.stop)] = {'gene_id':row.gene_id, 'exon_region_id':row.exon_region_id,'stop':row.stop}
+            ea = [row.chr,row.exon_id,row.start,row.stop]
+            if(row.gene_id in gene_model_exon_dict):
+                gene_model_exon_dict[row.gene_id].append(ea)
+            else:
+                gene_model_exon_dict[row.gene_id] = [ea]
         print("hello")
         logging.info("Finished generating junction map from gene model")
-
-        return junction_dict
+        return gene_model_dict
     
+   
+
+
+class Splice_Site_Annotation:
+
+    def __init__(self,chr,start,stop,startannotation,stopannotation):
+        self.chr = chr
+        self.start = start
+        self.stop = stop
+        self.startannotation = startannotation
+        self.stopannotation = stopannotation
+    
+    def start_annotation(self):
+        if self.stopannotation == None and self.stop_annotation() == None:
+            self.start_annotation = 'NA'
+        return self.start_annotation
+    
+    def stop_annotation(self):
+        if self.stop_annotation == None and self.start_annotation() == None:
+            self.stop_annotation = 'NA'
+        elif self.stop_annotation == None:
+            candidategene = self.find_gene(self.start_annotation())
+            self.stop_annot = self.splice_annotations(self.chr,self.stop,candidategene)
+        return self.stop_annot
+    
+    def find_gene(start_annotation):
+        return start_annotation['gene_id']
+    
+    
+
+
+
     def splice_annotations(self,start_ann,stop_ann):
         annotations = {}
-        annotation = start_ann['exon_region_id'] + '_' + start_ann['start']   
-        return annotations
+        junction_start = start_ann['junction_start']
+        junction_stop = stop_ann['junction_stop']
+        genemodel_stop = start_ann['ref_stop']
+        genemodel_start = stop_ann['ref_start']
+        inRange = self.coordinate_in_range(junction_start, junction_stop, genemodel_start,genemodel_stop)
+        if(inRange):
+            annotation = start_ann['exon_region_id'] + '_' + start_ann['junction_start']   
+        return annotation
     
+    def coordinate_in_range(self,start,stop,ref_start,ref_stop):
+        if(start == ref_start and stop == ref_stop):
+            return True
 
 gene_model_all = '/Users/sin9gp/altanalyze3/tests/data/gene_model_all.txt'
 junction_dir = '/Users/sin9gp/altanalyze3/tests/data/junction_dir/'
 junction_annot = JunctionAnnotation()
-junction_annot.each_junction_annotation(junction_dir=junction_dir,gene_model_all=gene_model_all)
-#junction_annot.generate_junction_dict('/Users/sin9gp/altanalyze3/tests/data/gene_model_all.txt')
+#junction_annot.each_junction_annotation(junction_dir=junction_dir,gene_model_all=gene_model_all)
+junction_annot.generate_gene_model_dict('/Users/sin9gp/altanalyze3/tests/data/gene_model_all.txt')
